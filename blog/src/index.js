af5fa054cea65f145975b8e193b35e28b9009518
Huynh Phuc <huynhhuuphuc100599@gmail.com>
2022-02-15 09:59:22 +0700

    Rafactor code: create routes, app/controllers

--- blog/src/index.js
+++ blog/src/index.js
@@ -5,6 +5,8 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 
 // HTTP logger
@@ -20,14 +22,15 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+// Routes init
+route(app);
 
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// req: request: gởi lên cái URL nó khớp vs tuyến đường ở backend. Nếu khớp sẽ lọt vào function handler sẽ nhận 1 cái biến là request, Biến request chứa tất cả thông tin liên quan đến yêu cầu gởi đi (vd từ biến req có thể lấy ra các thông tin ở request header)
+
+// res: response: server nhân request để xử lý xòng trả về 1 response render ra client (res nếu cần databse thì chọc vào dataabse, ...)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// Query parameters: Truyền dữ liệu qua chính URL của mình (cú pháp param là "?q="value" từ tham số thư 2 trở đi thì đổi dấu ? thành &ref="value" muốn thêm tham số nữa thì vấn tham số &author"value"")
